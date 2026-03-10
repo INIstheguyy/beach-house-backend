422a5bffc85fe19bc74d2230f5d2d274c4c2b954
INIstheguyy <Komolafeinioluwa9@gmail.com>
2026-03-10 15:59:08 +0100

    fix: env fix

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,11 +1,11 @@
-export default ({ env }) => ({
+export default () => ({
   upload: {
     config: {
       provider: "@strapi/provider-upload-cloudinary",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
       },
       actionOptions: {
         upload: {},
