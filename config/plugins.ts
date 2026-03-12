cd42c6ad5fb434b21aa3f5bf8a72d64abce47193
INIstheguyy <Komolafeinioluwa9@gmail.com>
2026-03-12 10:38:18 +0100

    feat: add Cloudinary upload provider configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,9 +3,9 @@ export default () => ({
     config: {
       provider: "@strapi/provider-upload-cloudinary",
       providerOptions: {
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET,
+        cloud_name: process.env.CLD_CLOUD_NAME,
+        api_key: process.env.CLD_API_KEY,
+        api_secret: process.env.CLD_API_SECRET,
       },
       actionOptions: {
         upload: {},
